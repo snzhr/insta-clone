afdc54242863f1d5b7cdaf66d2ce2d37c25c0f65
snzhr <snzhrski@gmail.com>
2021-12-29 14:55:49 +0600

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDO_zDM64ImITbDbYB7vTiIw_94H5C1lkY",
+  authDomain: "vuestagram-1f8ab.firebaseapp.com",
+  projectId: "vuestagram-1f8ab",
+  storageBucket: "vuestagram-1f8ab.appspot.com",
+  messagingSenderId: "769090292501",
+  appId: "1:769090292501:web:6100e9a30b59e85f76b71c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 createApp(App).use(store).use(router).mount('#app')
