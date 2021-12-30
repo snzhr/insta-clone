b3e673681b70f38b2979203ca1c26b355bda19e0
snzhr <snzhrski@gmail.com>
2021-12-30 13:23:50 +0600

    Navbar layout created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <Navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar.vue";
 export default {
   name: "Home",
+  components: {
+    Navbar,
+  },
 };
 </script>
